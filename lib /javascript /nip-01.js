7ca8cc06c58e67a84c8aa9bd246a2377a9d5cdef
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-19 09:42:50 -0300

    Update nip-01.js

--- lib /javascript /nip-01.js
+++ lib /javascript /nip-01.js
@@ -1,1 +1,20 @@
+// Event class based on NIP-01
+class Event {
+  constructor(name, metadata, timestamp, source, version) {
+    this.name = name;
+    this.metadata = metadata;
+    this.timestamp = timestamp;
+ 
+  }
+}
+
+// Example usage:
+const event = new Event(
+  "Transfer", // Event name
+  { from: "0x123abc", to: "0x456def", amount: 100 }, // Metadata object
+  Math.floor(Date.now() / 1000), // Timestamp in seconds
+  "1.0" // Event version
+);
+
+console.log(event);
 
